e6d3f64513f3a5eed69ae1dee47761cae9c5c086
wcw1998 <1365196568@qq.com>
2025-07-31 23:56:27 +0800

    使用idea提交代码

---
+++ src/main/java/com/atguigu/learngit/Main.java
@@ -0,0 +1,7 @@
+package com.atguigu.learngit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/atguigu/learngit/People.java
@@ -0,0 +1,7 @@
+package com.atguigu.learngit;
+
+public class People {
+    private String name;
+    private int age;
+    private String phoneNo;
+}
