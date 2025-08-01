cb019e52d3bfdb157e925c94a4fa7c3881264417
wcw1998 <1365196568@qq.com>
2025-08-01 19:17:18 +0800

    hot-fix comit first

--- src/main/java/com/atguigu/learngit/Main.java
+++ src/main/java/com/atguigu/learngit/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello, github!");
         System.out.println("Hello, git again!");
+        System.out.println("Hello, git 4!");
     }
 }
