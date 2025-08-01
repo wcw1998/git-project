947cd4d8b976ae45cab0e02b0fb9467c68f48988
wcw1998 <1365196568@qq.com>
2025-08-01 21:11:36 +0800

    你说的对，我把你说的都能打印出来

--- src/main/java/com/atguigu/gitdemo/GitTest.java
+++ src/main/java/com/atguigu/gitdemo/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello, github!");
         String str = new String("拽什么洋文呢？");
+        System.out.println(str);
     }
 }
