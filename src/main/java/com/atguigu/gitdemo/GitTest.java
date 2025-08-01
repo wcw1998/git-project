dacc59d73e81640a5a9d5a2c78d31bc516f8ff3d
wcw1998 <90839382+wcw1998@users.noreply.github.com>
2025-08-01 21:09:51 +0800

    Update GitTest.java
    我觉得一直说洋文不太好

--- src/main/java/com/atguigu/gitdemo/GitTest.java
+++ src/main/java/com/atguigu/gitdemo/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.gitdemo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello, github!");
+        String str = new String("拽什么洋文呢？");
     }
 }
